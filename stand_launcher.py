880e19d811e8701b1629301029820e6e2d42c252
centroid457 <centroid@mail.ru>
2020-11-19 15:01:06 +0300

    add buttons to control frame, only visual

--- stand_launcher.py
+++ stand_launcher.py
@@ -46,20 +46,37 @@ def main():
     root.geometry("300x200")
     root.overrideredirect(0)
 
+
+    # ==================== FRAME CONTROL ====================
     frame_control = Frame(root, bg="#505050", width=200, height=20)
     frame_control.pack(side='top', fill=BOTH, expand=0)
     '''Would Be great if it could be specified to only be moved
     when dragging with the Frame above.'''
     grip = Grip(frame_control)
 
-    button_exit = Button(frame_control, text="X", bg="#FF6666", fg="white", command=lambda: exit())
-    button_exit.pack(side='right')
+    button_window_exit = Button(frame_control, text="X", width=3, height=1, bg="#FF3333", fg="white", command=lambda: exit())
+    button_window_exit.pack(side='right')
+
+    button_window_fullscreen = Button(frame_control, text="^", width=3, height=1, bg="white", fg="black", command=lambda: exit())
+    button_window_fullscreen.pack(side='right')
+
+    button_window_down = Button(frame_control, text="_", width=3, height=1, bg="white", fg="black", command=lambda: exit())
+    button_window_down.pack(side='right')
+
+    button_window_topalways = Button(frame_control, text="top", width=3, height=1, bg="white", fg="black", command=lambda: exit())
+    button_window_topalways.pack(side='right')
+
+    button_window_settings = Button(frame_control, text="Настройки", height=1, bg="white", fg="black", command=lambda: exit())
+    button_window_settings.pack(side='right')
 
 
+    # ==================== FRAME MAIN ====================
     frame_back = Frame(root, bg="grey")
     frame_back.pack_propagate(0)
     frame_back.pack(side='top', fill=BOTH, expand=1)
 
     root.mainloop()
 
-main()
+
+if __name__ == '__main__':
+    main()
