02c73a853fe5770f2ace4d66ae0c1a755ca8e4ec
centroid457 <centroid@mail.ru>
2020-11-20 22:23:22 +0300

    resolve svitching color buttons! by collections deque!

--- stand_launcher.py
+++ stand_launcher.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from collections import deque
 
 
 # #################################################
@@ -54,11 +55,6 @@ class Grip:
 class Gui(tk.Frame):
     """ main GUI window """
     def __init__(self, master=None):
-        self.window_flag_topalways = False
-        self.window_flag_fullscreen = False
-        self.window_flag_independent = False
-        self.window_flag_frame_settings_open = False
-        self.color_button_set_normal = ("white", "#77FF77")
         self.window_state = ('normal', "zoomed")
 
         super().__init__(master)
@@ -154,59 +150,60 @@ class Gui(tk.Frame):
     # BUTTONS
     # #################################################
     def get_button_data(self):
+        color_button_normal_set = ["white", "#77FF77"]
         self.button_data = {
             "button_window_exit":{
-                "text" : "X",
-                "bg" : "#FF6666",
-                "command" : exit,
-                "side" : "left",
+                "text": "X",
+                "bg": deque(["#FF6666"]),
+                "command": exit,
+                "side": "left",
                 },
 
             "button_window_fullscreen":{
                 "text": "^",
-                "bg": self.color_button_set_normal[int(self.window_flag_fullscreen)],
+                "bg": deque(color_button_normal_set),
                 "command": self.window_control_fullscreen,
                 "side": "left",
                 },
 
             "button_window_down":{
                 "text": "_",
-                "bg": "white",
+                "bg": deque(["white"]),
                 "command": lambda: self.master.iconify(),
                 "side": "left",
                 },
 
             "button_window_moveto00": {
                 "text": "(0.0)",
-                "bg": self.color_button_set_normal[int(self.window_flag_topalways)],
+                "bg": deque(["white"]),
                 "command": lambda: self.create_window_geometry(moveto00=True),
                 "side": "left",
                 },
 
             "button_window_set_as_started": {
                 "text": "begin",
-                "bg": self.color_button_set_normal[int(self.window_flag_topalways)],
+                "bg": deque(["white"]),
                 "command": self.create_window_geometry,
                 "side": "left",
                 },
 
             "button_window_topalways": {
                 "text": "top",
-                "bg": self.color_button_set_normal[int(self.window_flag_topalways)],
+                "bg": deque(color_button_normal_set),
                 "command": self.window_control_top,
                 "side": "left",
                 },
 
             "button_window_independent": {
                 "text": "I",
-                "bg": self.color_button_set_normal[int(self.window_flag_independent)],
+                "bg": deque(color_button_normal_set),
                 "command": self.window_control_independent,
                 "side": "left",
                 },
 
             "button_window_settings": {
                 "text": "Настройки",
-                "bg": "white",
+                "bg": deque(color_button_normal_set),
                 "command": self.frame_settings_open,
                 "side": "left",
                 },
@@ -221,25 +218,25 @@ class Gui(tk.Frame):
         btn = tk.Button(frame)
         btn["text"]=self.button_data[button_type]["text"]
         btn["width"]=3 if len(btn["text"]) < 3 else None
-        btn["bg"]=self.button_data[button_type]["bg"]
+        btn["bg"]=self.button_data[button_type]["bg"][0]
         btn.bind("<Button-1>", self.buttons_handle)
         btn.pack(side=self.button_data[button_type]['side'])
 
     def buttons_handle(self, event):
         for button_id in self.button_data:
             if self.button_data[button_id]["text"] == event.widget["text"]:
+                self.button_data[button_id]["bg"].rotate()
+                event.widget["bg"]=self.button_data[button_id]["bg"][0]
                 self.button_data[button_id]["command"]()
                 return
 
     # BUTTON FUNCTIONS
     def window_control_top(self, setdefault=False):
         self.window_flag_topalways = 0 if setdefault else not (self.window_flag_topalways)
-        #self.button_window_topalways["bg"] = self.color_button_set_normal[int(self.window_flag_topalways)]
         self.master.wm_attributes("-topmost", self.window_flag_topalways)
 
     def window_control_fullscreen(self, setdefault=False):
         self.window_flag_fullscreen = 0 if setdefault else not (self.window_flag_fullscreen)
-        #self.button_window_fullscreen["bg"] = self.color_button_set_normal[int(self.window_flag_fullscreen)]
         self.master.state(self.window_state[int(self.window_flag_fullscreen)])
         if not self.window_flag_fullscreen:
             self.master.wm_attributes('-fullscreen', self.window_flag_fullscreen)
@@ -247,12 +244,10 @@ class Gui(tk.Frame):
     def window_control_independent(self, setdefault=False):
         """make window independent from OS explorer"""
         self.window_flag_independent = 0 if setdefault else not (self.window_flag_independent)
-        #self.button_window_independent["bg"] = self.color_button_set_normal[int(self.window_flag_independent)]
         self.master.wm_overrideredirect(self.window_flag_independent)
 
     def frame_settings_open(self, setdefault=False):
         self.window_flag_frame_settings_open = 0 if setdefault else not (self.window_flag_frame_settings_open)
-        #self.button_window_settings["bg"] = self.color_button_set_normal[int(self.window_flag_frame_settings_open)]
         if self.window_flag_frame_settings_open:
             self.frame_settings.grid()
         else:
