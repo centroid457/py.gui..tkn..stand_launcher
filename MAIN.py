dd5a08a5e439e53d01427177ba7349e33f627bb5
centroid457 <centroid@mail.ru>
2020-12-15 18:00:29 +0300

    zero

--- MAIN.py
+++ MAIN.py
@@ -1,13 +1,1 @@
-from tkinter import Tk
-
-from import_checker import main, python_files_found_in_directory_list, ranked_modules_dict
-#print(ranked_modules_dict)      #{}
-main(__file__)
-#print(ranked_modules_dict)      #{'import_checker': [True, 'py.gui..tkn..stand_launcher', None], }
-
-from import_checker_gui_set import *
-
-from import_checker_gui import *
-root = Tk()
-app = Gui(root=root)
-app.mainloop()
+import import_checker
