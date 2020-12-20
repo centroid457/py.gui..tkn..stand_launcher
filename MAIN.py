37f1edf4b6bc8718beb4ca8745651cb7f34fd3cd
centroid457 <centroid@mail.ru>
2020-12-21 00:16:10 +0300

    fix import checker position!

--- MAIN.py
+++ MAIN.py
@@ -1,7 +1,11 @@
+# ##########################################################
+# DO NOT CORRECT THIS ORDER! PLACE YOUR ALL IMPORTS AFTER!!!
 import sys
 sys.path.append("C:\\!CENTROID\\ProjectsPYTHON\\!MODULES\\")
-
 import import_checker
 import_checker.frame.main(__file__)
 import stand_launcher
 stand_launcher.main()
+# ##########################################################
+
+#import test123
